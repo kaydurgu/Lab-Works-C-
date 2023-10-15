46027013c5e00b67887e6c21a835cea719e78ada
kaydurgu <mzhanbolot@gmail.com>
2023-10-15 18:05:40 +0300

    Lab 2

---
+++ LAB2.1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB2._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string m = (string)(sender as Button).Tag;
+            MessageBox.Show(m);
+            int k = Convert.ToInt32((sender as Button).Tag);
+            string s = "You pressed the button";
+            switch (k)
+            {
+                case 1:
+                    s += "number 1";
+                    break;
+                case 2:
+                    s += "number 2";
+                    break;
+                default:
+                    s = "you couldn't get here";
+                    break;
+            }
+            MessageBox.Show(this, s, "Which button?");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k = Convert.ToInt32((sender as Button).Tag);
+            MessageBox.Show(k.ToString());
+            MessageBox.Show(this, (sender as Button).Text, "Button 1");
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int  i = 0; i < this.Controls.Count; i++){
+                if (Convert.ToString(this.Controls[i].GetType().Name) == "Button")
+                {
+                    MessageBox.Show(this, Convert.ToString(this.Controls[i].Name),
+                        "What buttons are there");
+                }
+
+            }           
+        }
+    }
+}
---
+++ LAB2.1/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB2._1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with a line");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with line", "Line Title");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with a string and different buttons", "Title string", MessageBoxButtons.OKCancel);
+            MessageBox.Show("Dialog box with a line and different buttons", "Title line", MessageBoxButtons.AbortRetryIgnore);
+            MessageBox.Show("Dialog box with a line and different buttons", "Title line", MessageBoxButtons.OK);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with a line and different buttons", "Title bar", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            MessageBox.Show("Dialog box with a line and different buttons", "Title line", MessageBoxButtons.OKCancel, MessageBoxIcon.Stop);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Save data", "Attention!",
+                MessageBoxButtons.YesNoCancel);
+            if (result == DialogResult.Yes){
+                MessageBox.Show("Data Saved", "Data Saved");
+            }
+            if (result == DialogResult.No)
+            {
+                MessageBox.Show("Data not Saved", "Data not Saved");
+            }
+            if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show("Saving data canceled by the user", "Data not Saved");
+            }
+
+        }
+    }
+}
